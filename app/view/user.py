4dfb9c36f4a1fb676c44b08332edfb21a3eeddfd
duanchangbiao@zhangkongapp.com <duan123456@>
2025-01-08 18:24:47 +0800

    添加用户更新功能

--- app/view/user.py
+++ app/view/user.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 from flask import Blueprint, request
 from app.common.helper import ModelFilter
 from app.extensions import db
 from app.models.models import User, UserRole, Role
-from app.utils.response import table_api, success_api
+from app.utils.response import table_api, success_api, fail_api
 
 app_router = Blueprint('user', __name__, url_prefix="/systemManage")
 
@@ -11,11 +13,11 @@ app_router = Blueprint('user', __name__, url_prefix="/systemManage")
 def getUserList():
     page = request.args.get('current', type=int)
     size = request.args.get('size', type=int)
-    username = request.args.get("username")
-    nickname = request.args.get("nickname")
-    email = request.args.get("email")
+    username = request.args.get("userName")
+    nickname = request.args.get("nickName")
+    email = request.args.get("userEmail")
     active = request.args.get("isActive")
-    status = request.args.get("status")
+    status = request.args.get("userStatus")
     mf = ModelFilter()
     if username:
         mf.like("username", username)
@@ -35,18 +37,19 @@ def getUserList():
             .paginates(page=page, pageSize=size))
     total = user.total
     return table_api(data=[{
-        "username": user.username if user.username is not None else None,
-        "nickname": user.nickname if user.nickname is not None else None,
-        "email": user.email if user.email is not None else None,
-        "sex": user.sex if user.sex is not None else None,
-        "phone": user.phone if user.phone is not None else None,
-        "remark": user.remark if user.remark is not None else None,
-        "status": user.status if user.status is not None else None,
+        'id': user.id if user.id is not None else None,
+        "userName": user.username,
+        "nickName": user.nickname,
+        "userEmail": user.email,
+        "userGender": user.sex,
+        "userPhone": user.phone,
+        "remark": user.remark,
+        "status": str(user.status),
         "isActive": int(user.is_active),
         "userRole": {
-            "roleName": role.role_name if role.role_name is not None else None,
-            "roleCode": role.role_code if role.role_code is not None else None,
-            "roleId": role.role_id if role.role_id is not None else None,
+            "roleName": role.role_name,
+            "roleCode": role.role_code,
+            "roleId": role.role_id,
         },
         "createTime": user.ctime.strftime("%Y-%m-%d %H:%M:%S") if user.ctime is not None else None,
         "updateTime": user.mtime.strftime("%Y-%m-%d %H:%M:%S") if user.mtime is not None else None
@@ -67,3 +70,31 @@ def saveUser():
     return success_api()
 
 
+@app_router.route("/updateUser", methods=["POST"])
+def updateUserInfo():
+    id = request.get_json().get("id")
+    username = request.get_json().get("userName")
+    password = request.get_json().get("password")
+    nickname = request.get_json().get("nickName")
+    email = request.get_json().get("userEmail")
+    phone = request.get_json().get("userPhone")
+    isActive = request.get_json().get("isActive")
+    status = int(request.get_json().get("status"))
+    userRoleParams = request.get_json().get("userRoles")
+    user = User(id=id, username=username, password=password, nickname=nickname, email=email, phone=phone,
+                is_active=isActive, status=status)
+    if bool(UserRole.query.filter_by(user_id=id).count()):
+        UserRole.query.filter_by(user_id=id).delete()
+    User.query.filter_by(id=id).update({"is_active": user.is_active,
+                                        "status": user.status,
+                                        'username': user.username,
+                                        'email': user.email,
+                                        'phone': user.phone,
+                                        'nickname': user.nickname,
+                                        'password': user.password,
+                                        'mtime': datetime.now()
+                                        })
+    userRole = UserRole(user_id=id, role_id=userRoleParams[0], ctime=user.ctime)
+    db.session.add(userRole)
+    db.session.commit()
+    return success_api(msg='修改成功!')
